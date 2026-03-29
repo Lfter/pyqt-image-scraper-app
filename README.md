bac9cf2a05ba87726e19dcb9c30e1f8a70acf1e0
Ling <2496104518@qq.com>
2026-03-29 23:39:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PyQt Image Scraper App
+
+一个使用 PyQt 开发的网页图片抓取工具。
+
+## 功能
+- 输入网页地址
+- 选择图片保存目录
+- 抓取网页图片并分类保存
+- 显示抓取进度
+
+## 运行方式
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python "Pyqt Image Scraper App.py"
---
+++ run_app.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export QT_QPA_PLATFORM_PLUGIN_PATH="/home/ling/桌面/Python/Pyqt Image Scraper App/.venv/lib/python3.12/site-packages/PyQt5/Qt5/plugins/platforms"
+exec "/home/ling/桌面/Python/Pyqt Image Scraper App/.venv/bin/python" "/home/ling/桌面/Python/Pyqt Image Scraper App/Pyqt Image Scraper App.py"
